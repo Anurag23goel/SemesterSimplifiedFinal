b11119e5404cbc6484de1977e63be83a993fbe4e
Anurag23goel <anurag.go3l07@gmail.com>
2024-11-21 17:36:55 +0530

    Changed Origin

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://192.168.1.10:3000", "https://semester-simplified-backend.onrender.com/"], // Specify your frontend URL(s)
+    origin: true, // Specify your frontend URL(s) ["http://localhost:3000", "http://192.168.1.10:3000", "https://semester-simplified-backend.onrender.com/"]
     methods: ["GET", "POST", "PUT", "DELETE"], // Include all necessary methods
     credentials: true, // Allow credentials (cookies)
   },
